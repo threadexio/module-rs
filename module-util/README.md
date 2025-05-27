fbd53ee3f06b4ccf030a283243a9033731fcaf01
threadexio <pzarganitis@gmail.com>
2025-05-27 15:35:33 +0300

    docs: add `module-util/README.md`

---
+++ module-util/README.md
@@ -0,0 +1,3 @@
+# module-util
+
+Various utilities for working with [`module`](https://github.com/threadexio/module-rs).
--- module-util/src/lib.rs
+++ module-util/src/lib.rs
@@ -1,3 +1,7 @@
-//! Various utilities for working with modules.
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/threadexio/module-rs/master/assets/icon.png"
+)]
+#![doc = include_str!("../README.md")]
+#![cfg_attr(module_nightly, feature(doc_auto_cfg))]
 
 pub mod file;
